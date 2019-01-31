fd39ff27021150676b90e0dc99168a17554ea039
Sam Miller <sammillerks@gmail.com>
2019-01-31 06:20:00 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playlist = {
   acdc: "Back in Black",
   thedoors: "Riders on the Storm",
@@ -14,3 +15,25 @@ function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
   return playlist;
 }
+=======
+var meals = {};
+
+var meals = new Object();
+
+var meals = {breakfast:"oatmeal"};
+
+var meals = new Object ({breakfast:'oatmeal'})
+
+const firstMeal = 'breakfast'
+
+var meals = { [firstMeal]: 'oatmeal' }
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+  return obj
+}
+
+const recipe = { eggs: 3 }
+
+destructivelyUpdateObjectWithKeyAndValue(recipe, 'flour', '3 cups')
+>>>>>>> 437caa07c22bc659aad323d41abd983293955f3e
